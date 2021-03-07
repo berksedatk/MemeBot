bde465acf8da4ad986fa68073128912b679e5367
SaxDev <berksedat123@gmail.com>
2021-03-07 14:59:36 +0300

    t

--- commands/misc/unbox.js
+++ commands/misc/unbox.js
@@ -6,6 +6,8 @@ module.exports = {
   description: "Unboxes your limited role!",
   cooldown: 60,
   execute(bot, message, args) {
+    if (message.channel.id != "536958289641078804") return;
+
     let items = [
       "587345073792876564", "588479443710902282", "591698070278897704", "596559577856606218", "598680911189508108",
       "610216776860696576", "615009100324929620", "612135250767642664", "622423504595320852", "624704663962255370",
@@ -40,6 +42,8 @@ module.exports = {
           if (item === "816431917712932914") {
             return message.member.roles.add(items[35], {reason: `They won it!!`}).then(() => {
               message.channel.send({embed:{color: "GOLD", description: `Congrats!! You got the super rare role <@&${items[35]}>!` }})
+              bot.channels.cache.get("724067529605120051").send({embed:{color: "GOLD", description: `${message.author} unboxed the super rare role <@&${items[35]}>!` }})
+              bot.channels.cacae.get("").send({embed:{color: "GOLD", description: `${message.author} unboxed the super rare role <@&${items[35]}>!` }})
               if (stats.roles.get(items[35])) {
                 let count = stats.roles.get(items[35]).count ++;
                 stats.roles.set(items[35], {
@@ -52,6 +56,7 @@ module.exports = {
         }
         message.member.roles.add(item, {reason: `They won it`}).then(() => {
           message.channel.send({embed:{color: "PURPLE", description: `You unboxed the <@&${item}> role!` }});
+          bot.channels.cacae.get("").send({embed:{color: "PURPLE", description: `${message.author} unboxed the role <@&${items[35]}>` }})
           if (stats.roles.get(item)) {
             let count = stats.roles.get(item).count ++;
             stats.roles.set(item, {
