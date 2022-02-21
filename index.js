3311a1b4e673fab8a361aa33f0ccdb615c3f3be6
SaxDev <berksedat123@gmail.com>
2022-02-21 19:58:33 +0300

    Yes

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const fs = require("fs");
 const { prefix } = require("./config.json");
 require('dotenv').config();
 
-//MongoDB
-const mongoose = require('mongoose');
-(async () => {
-  await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log("Mongoose - Connected to database successfully.");
-  }).catch(err => {
-    console.error("Mongoose - Could not connect to the database! " + err);
-  });
-})();
-
 //Discord
 const { Client, Collection } = require("discord.js");
 const client = new Client({
