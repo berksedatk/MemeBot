4288e4329e26fd2b301a8db7280e1bceb368aad6
SaxDev <berksedat123@gmail.com>
2022-02-21 20:38:17 +0300

    b

--- schemas/starboard.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const starSchema = new mongoose.Schema({
-  userID: String,
-  username: String,
-  stars: String
-});
-
-module.exports = mongoose.model("starboard", starSchema);
