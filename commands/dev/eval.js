b0cbab5eac05282f0ec2cc17563066ceb1e39bee
SaxDev <berksedat123@gmail.com>
2022-02-21 20:47:48 +0300

    d

--- commands/dev/eval.js
+++ commands/dev/eval.js
@@ -47,7 +47,7 @@ module.exports = {
           .addField("Input", `\`\`\`js\n${code}\`\`\``)
           .addField("Output", `\`\`\`js\n${output}\`\`\``)
           .setColor("#fcfffd");
-        msg.edit("", embed);
+        msg.edit("Results", embed);
       }
     } catch (e) {
       msg.edit(`Error \`\`\`js\n${e}\`\`\``);
