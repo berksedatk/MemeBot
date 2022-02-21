f5659ac8af7a7ecf8744950594cf890890aae652
SaxDev <berksedat123@gmail.com>
2022-02-21 23:06:49 +0300

    sdfsf

--- commands/dev/eval.js
+++ commands/dev/eval.js
@@ -38,7 +38,7 @@ module.exports = {
           .setURL(haste)
           .setColor("#fcfffd");
           console.log(output);
-          msg.edit({content: null, embeds: [embed]});
+          msg.edit({content: "Heres code output", embeds: [embed]});
         }).catch(err => {
           console.log("Hastebin error: " + err)
         });
@@ -47,7 +47,7 @@ module.exports = {
           .addField("Input", `\`\`\`js\n${code}\`\`\``)
           .addField("Output", `\`\`\`js\n${output}\`\`\``)
           .setColor("#fcfffd");
-        msg.edit({content: null, embeds: [embed]} );
+        msg.edit({content: "Heres code output", embeds: [embed]} );
       }
     } catch (e) {
       msg.edit(`Error \`\`\`js\n${e}\`\`\``);
