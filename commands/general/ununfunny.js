b1d7076a4ead1c2f1ad8ecda7e6bf101af1f4a7e
SaxDev <berksedat123@gmail.com>
2022-04-01 00:34:56 +0300

    fixed fools

--- commands/general/ununfunny.js
+++ commands/general/ununfunny.js
@@ -8,14 +8,14 @@ module.exports = {
   execute(bot, message, args) {
     let roll = Math.floor(Math.random() * 10);
     if (message.channel.id == "955477818022715442" && message.member.roles.cache.has("955854711112728576")) {
-        if (roll = 1) {
+        if (roll == 1) {
             message.member.roles.add("956020022675923005").then(() => {
                 message.member.roles.remove("955854711112728576").then(() => {
-                  message.channel.send(message.author + " escaped unfunny jail.");
+                  message.channel.send(`<@${message.author}> escaped unfunny jail.`);
                 })
             });
         } else {
-            message.channel.send(message.author + " No lmao");
+            message.channel.send(`<@${message.author}> No lmao`);
         }
     }
     message.delete();
